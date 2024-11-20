26273fc01de945a66f2b7f1895e9cef90e649a34
Thotapavankumar-1710 <pavankumarthota1710@gmail.com>
2024-11-20 13:33:25 +0530

    Add files via upload

---
+++ major/register.php
@@ -0,0 +1,35 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $mobile_number = $_POST['mobile_number'];
+    $gender = $_POST['gender'];
+    $password = $_POST['password'];
+    $confirm_password = $_POST['confirm_password'];
+
+    if ($password !== $confirm_password) {
+        die("Passwords do not match! <a href='register.html'>Try again</a>");
+    }
+
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    // Database connection
+    $conn = new mysqli('localhost', 'root', '', 'shop_db');
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $stmt = $conn->prepare("INSERT INTO users (username, email, mobile_number, gender, password) VALUES (?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssss", $username, $email, $mobile_number, $gender, $hashed_password);
+
+    if ($stmt->execute()) {
+        echo "Registration successful! <a href='login.html'>Login here</a>";
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+?>
